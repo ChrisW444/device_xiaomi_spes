9e629b6b9738fa8193ac41337924492b840698b6
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-04-02 20:43:03 +0000

    m
    Change-Id: I10d1404e85868a4ebeca317340b7d7838837d692
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- ultrasound/libnotifyaudiohal.cpp
+++ ultrasound/libnotifyaudiohal.cpp
@@ -15,25 +15,25 @@ using android::hardware::audio::V6_0::IDevicesFactory;
 using android::hardware::audio::V6_0::IPrimaryDevice;
 using android::hardware::audio::V6_0::Result;
 
-void ultrasound_enable(int enable) {
-    ALOGD("ultrasound_enable: %d", enable);
+void elliptic_ultrasound_supported(int enable) {
+    ALOGD("elliptic_ultrasound_supported: %d", enable);
     auto factory = IDevicesFactory::getService();
     factory->openPrimaryDevice([&](Result retval, const sp<IPrimaryDevice>& result) {
         if (retval == Result::OK) {
             result->setParameters({} /* context */,
                                   {
-                                          {"ultrasound-sensor", std::to_string(enable)},
+                                          {"ultrasound-proximity", std::to_string(enable)},
                                   });
         }
     });
 }
 
 extern "C" void elliptic_notify_audio_hal(char* param) {
-    ALOGD("elliptic_notify_audio_hal: %s", param);
-    if (!strcmp(param, "ultrasound_enable=1")) {
-        ultrasound_enable(1);
-    } else if (!strcmp(param, "ultrasound_enable=0")) {
-        ultrasound_enable(0);
+    ALOGD("elliptic_notify_audio_hal_hidl: %s", param);
+    if (!strcmp(param, "elliptic_ultrasound_supported=1")) {
+        elliptic_ultrasound_supported(1);
+    } else if (!strcmp(param, "elliptic_ultrasound_supported=0")) {
+        elliptic_ultrasound_supported(0);
     }
 }
 
